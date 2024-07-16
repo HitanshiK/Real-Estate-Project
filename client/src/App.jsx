3582e66adb2c7c189ab6b7670e3f2745c5a2f739
Hitanshik <hitanshikothari2003@gmail.com>
2024-07-16 23:24:20 +0530

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import SignIn from './pages/SignIn';
-import SignUp from './pages/SignUp';
+import SignIn from './pages/Signin';
+import SignUp from './pages/Signup';
 import About from './pages/About';
-import Profile from './pages/Profile';
+import Profile from './pages/profile';
 import Header from './components/Header';
 import PrivateRoute from './components/PrivateRoute';
-import CreateListing from './pages/CreateListing';
+import CreateListing from './pages/createListing';
 import UpdateListing from './pages/UpdateListing';
 import Listing from './pages/Listing';
 import Search from './pages/Search';
